89f63d8ae52bfec75133c6cfd05b9b8444a751cf
lensu <1217079729@qq.com>
2020-10-20 19:32:38 +0800

    Update wolong.js

--- media/wolong.js
+++ media/wolong.js
@@ -1,7 +1,5 @@
 var body = $response.body;
-if (url.indexOf(vip) != -1) {
-    let obj = JSON.parse(body);
-    obj.data.vip.isVip = 1;
-	body = JSON.stringify(obj);
- }
- $done({body})
+let obj = JSON.parse(body);
+obj.data.vip.isVip = 1;
+body = JSON.stringify(obj);
+$done({body})
