b45bc34ebbcd3c5908763c99358050b0afa6174c
米斯特张 <1217079729@qq.com>
2020-10-21 09:56:52 +0800

    更新配置

--- media/wolong.js
+++
@@ -1,7 +0,0 @@
-var body = $response.body;
-let obj = JSON.parse(body);
-obj.data.vip.isVip = 1;
-obj.data.vip.vipStart = "2020-01-01";
-obj.data.vip.vipEnd = "2022-01-31";
-body = JSON.stringify(obj);
-$done({body})
