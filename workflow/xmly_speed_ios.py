0d76d89fadbe46008d11964a5d7a5f3d6f20c124
lensu <1217079729@qq.com>
2020-12-14 21:18:34 +0800

    Update xmly_speed_ios.py

--- workflow/xmly_speed_ios.py
+++ workflow/xmly_speed_ios.py
@@ -57,7 +57,7 @@ if "XMLY_SPEED_COOKIE" in os.environ:
 # 自定义设备命名,非必须 ;devices=["iPhone7P","huawei"];与cookiesList对应
 devices = []
 notify_time = 19                            # 通知时间,24小时制,默认19
-XMLY_ACCUMULATE_TIME = 1                    # 希望刷时长的,此处置1,默认打开;关闭置0
+XMLY_ACCUMULATE_TIME = 0                    # 希望刷时长的,此处置1,默认打开;关闭置0
 UserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 iting/1.0.12 kdtunion_iting/1.0 iting(main)/1.0.12/ios_1"
 # 非iOS设备的需要的自行修改,自己抓包 与cookie形式类似
 
