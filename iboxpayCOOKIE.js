bc984ac629d28eec3533d04a20086818004cc9f0
lensu <1217079729@qq.com>
2021-02-02 12:15:52 +0800

    updat

---
+++ iboxpayCOOKIE.js
@@ -0,0 +1,17 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let iboxpayheaderVal= ``
+let refreshtokenVal= ``
+
+
+let iboxpaycookie = {
+  iboxpayheaderVal: iboxpayheaderVal,  
+  refreshtokenVal: refreshtokenVal,  
+  
+}
+
+module.exports =  iboxpaycookie
+  
+
+
